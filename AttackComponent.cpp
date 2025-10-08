c0c1adce857e63f8cd1b209a767bb3284c72df72
DESKTOP08\kapib <kapirei08@outlook.jp>
2025-10-08 19:52:10 +0900

    文字化け修正

--- AttackComponent.cpp
+++ AttackComponent.cpp
@@ -19,16 +19,16 @@ AttackComponent::AttackComponent(Actor* owner)
 void AttackComponent::update()
 {
 	if (mActive) {
-		/* �ｽU�ｽ�ｽ�ｽI�ｽ�ｽ�ｽ�ｽ�ｽ�ｽ */
+		/* 攻撃終了判定 */
 		mTimer += GetFrameTime();
-		// �ｽU�ｽ�ｽ�ｽ�ｽ�ｽﾔゑｿｽ�ｽﾟゑｿｽ�ｽ�ｽ�ｽ�ｽ�ｽactive�ｽ�ｽ
+		// 攻撃時間を過ぎたら非activeに
 		if (mTimer > mCurInfo->duration) {
 			mActive = false;
 		}
-	
+
 		switch (mCurInfo->targetType)
 		{
-		// �ｽG�ｽﾖの攻�ｽ�ｽ�ｽﾌ場合
+			// 敵への攻撃の場合
 		case Actor::Type::Eenemy:
 			processAttackEnemy();
 			break;
@@ -36,28 +36,33 @@ void AttackComponent::update()
 			processAttackPlayer();
 			break;
 		}
-		//kb move
-		for (int i = mKnockbackTargets.size() - 1; i >= 0; --i) {
-			auto& info = mKnockbackTargets[i];
+	}
+	//kb move
+	for (int i = mKnockbackTargets.size() - 1; i >= 0; --i) {
+		auto& info = mKnockbackTargets[i];
 
-			//ターゲットが有効かチェック
-			if (info.target->getState() == Actor::Edead) {
-				mKnockbackTargets.erase(mKnockbackTargets.begin() + i);
-				continue;
-			}
-			info.timer -= GetFrameTime();
+		//ターゲットが有効かチェック
+		if (info.target->getState() == Actor::Edead) {
+			mKnockbackTargets.erase(mKnockbackTargets.begin() + i);
+			continue;
+		}
+		info.timer -= GetFrameTime();
 
-			if (info.timer <= 0.0f) {
-				mKnockbackTargets.erase(mKnockbackTargets.begin() + i);
-			}
-			else {
-				Vector2 currentPos = info.target->getPosition();
-				Vector2 moveAmount = Vector2Scale(info.velocity, GetFrameTime());
-				Vector2 newPos = Vector2Add(currentPos, moveAmount);
+		if (info.timer <= 0.0f) {
+			mKnockbackTargets.erase(mKnockbackTargets.begin() + i);
+		}
+		else {
+			// TODO: タスク2
+			/* ------ ノックバック中の位置更新 開始------ */
+			Vector2 currentPos = info.target->getPosition();
+			info.velocity.y += 50.0f;
+			Vector2 moveAmount = Vector2Scale(info.velocity, GetFrameTime());
+			Vector2 newPos = Vector2Add(currentPos, moveAmount);
 
-				info.target->setPosition(newPos);
-				info.target->computeRectangle();
-			}
+			// NOTE: 以下弄らなくてよい
+			info.target->setPosition(newPos);
+			info.target->computeRectangle();
+			/* ------ 敵のノックバック中の位置更新 終了------ */
 		}
 	}
 }
@@ -72,22 +77,10 @@ void AttackComponent::startAttack(AttackInfo* info)
 
 void AttackComponent::processAttackEnemy()
 {
-	// �ｽ謦｣�ｽ�ｽ�ｽﾄ敵�ｽ�ｽ�ｽ�ｽ�ｽ�ｽ�ｽﾄゑｿｽ�ｽ�ｽB�ｽ{�ｽ�ｽ�ｽ�ｽActorManager�ｽ�ｽ�ｽ邇晢ｿｽ�ｽ�ｽﾄゑｿｽ�ｽ�ｽﾝゑｿｽ�ｽ�ｽ�ｽﾈ仕�ｽg�ｽﾝにゑｿｽ�ｽ�ｽ�ｽ�ｽ�ｽ@
 	std::vector<EnemyActor*> enemies = 
 		static_cast<GamePlay*>(mOwner->getSequence())->getEnemies();
 	for (auto enemy : enemies) {
-		// �ｽ�ｽ�ｽ�ｽ �ｽF�ｽX�ｽ�ｽ竄ｪ�ｽ�ｽ�ｽ�ｽ
-		//
-		// 1.�ｽm�ｽb�ｽN�ｽo�ｽb�ｽN�ｽ�ｽﾔゑｿｽ�ｽ�ｽﾈゑｿｽ�ｽG�ｽﾉは難ｿｽ�ｽﾄなゑｿｽ
-		//  �ｽﾈ単�ｽ�ｽ�ｽ�ｽ,�ｽm�ｽb�ｽN�ｽo�ｽb�ｽN�ｽ�ｽ�ｽﾔゑｿｽ�ｽZ�ｽ�ｽ�ｽ�ｽ�ｽﾆゑｿｽ�ｽO�ｽ�ｽB�ｽ�ｽ閧�ｽ�ｽ�ｽ�ｽ�ｽ�ｽ轤ｵ�ｽ�ｽ�ｽﾈゑｿｽC�ｽ�ｽ�ｽ�ｽ�ｽ�ｽ
-		// 2.�ｽq�ｽb�ｽg�ｽ�ｽ�ｽX�ｽg
-		//  �ｽ�ｽﾔ暦ｿｽ�ｽz�ｽﾅはゑｿｽ�ｽ�ｽB�ｽG�ｽ�ｽ�ｽ�ｽ�ｽ�ｽ�ｽo�ｽ�ｽ�ｽ�ｽ�ｽ�ｽ�ｽ�ｽﾈゑｿｽO(n^2)�ｽ�ｽ�ｽC�ｽﾉなるか�ｽ�ｽ(�ｽ�ｽ�ｽ�ｽ�ｽ蛯ｵ�ｽﾄ変ゑｿｽ�ｽﾈゑｿｽ�ｽ�ｽ�ｽﾍゑｿｽ�ｽ�ｽ)
-		//  �ｽﾇ記�ｽF�ｽq�ｽb�ｽg�ｽ�ｽ�ｽX�ｽg�ｽ�ｽ�ｽﾌ敵�ｽ�ｽdelete�ｽ�ｽ�ｽ黷ｽ�ｽ�ｽ�ｽﾌ擾ｿｽ�ｽ�ｽ�ｽ�ｽ�ｽK�ｽv
-		// 3.�ｽU�ｽ�ｽ�ｽt�ｽ�ｽ�ｽ[�ｽ�ｽ�ｽ�ｽ�ｽﾉ誰�ｽ�ｽ�ｽﾉ難ｿｽ�ｽ�ｽ�ｽ�ｽ�ｽ�ｽ�ｽ�ｽ,mActive = false�ｽﾉゑｿｽ�ｽ�ｽ
-		//  �ｽ皷橸ｿｽ�ｽ�ｽ�ｽ�ｽﾉ包ｿｽ�ｽ�ｽ�ｽﾉ難ｿｽ�ｽﾄゑｿｽ�ｽ�ｽﾌゑｿｽ�ｽ�ｽ�ｽ�ｽ,�ｽm�ｽb�ｽN�ｽo�ｽb�ｽN�ｽ�ｽ�ｽﾌ敵�ｽﾉゑｿｽ�ｽ�ｽ�ｽﾄゑｿｽ�ｽ�ｽ
-		//  �ｽc�ｽ闕�ｿｽI�ｽﾈ攻�ｽ�ｽ�ｽ�ｽ�ｽ閧ｪ�ｽﾈゑｿｽ�ｽﾌで、�ｽ_�ｽb�ｽV�ｽ�ｽ�ｽU�ｽ�ｽ�ｽﾌ具ｿｽ�ｽ�ｽ�ｽﾍ心�ｽz
 
-		// ノックバック状態がまだないので,とりあえず3を採用しました
 		if (enemy->getHpComp()->isInvincible()) {
 			continue;
 		}
@@ -119,21 +112,17 @@ void AttackComponent::processAttackEnemy()
     // コンフリクト解消の都合で,とりあえず
 		if (CheckCollisionRecs(enemy->getRectangle(), mCurInfo->colRect)) {
 			if (auto boss = dynamic_cast<Boss*>(enemy)) {
-				// 笘 Boss 縺縺代�繧ｬ繝ｼ繝峨ｒ閠��縺励◆繝繝｡繝ｼ繧ｸ驕ｩ逕ｨ
+				// Boss クラスにダメージを与える処理
 				boss->ApplyDamage(mCurInfo->damage, mCurInfo->tag);
-				// �亥偵ｌ蛻､螳壹�Boss蛛ｴ/HP蛛ｴ縺ｧ蜃ｦ逅�ゅ％縺薙〒setState縺ｯ荳崎ｦ�ｼ
+				// もしBossが倒れた場合/HPが0になった場合はsetStateを呼ぶ
 			}
 		}
 
-		// �ｽﾈゑｿｽ�ｽA�ｽG�ｽ�ｽ�ｽG�ｽﾉ攻�ｽ�ｽ�ｽ�ｽ�ｽ骼厄ｿｽ�ｽ�ｽﾂ能�ｽﾈはゑｿｽ
-		// �ｽ�ｽ�ｽ�ｽ�ｽ�ｽ�ｽg�ｽ�ｽ�ｽU�ｽ�ｽ�ｽ�ｽ�ｽ�ｽ�ｽ�ｽ�ｽﾈゑｿｽ�ｽﾈゑｿｽ,component�ｽ�ｽowner�ｽﾆは難ｿｽ�ｽ�ｽ�ｽ�ｽﾈゑｿｽ�ｽ謔､�ｽﾉゑｿｽ�ｽ�ｽ�ｽﾉ擾ｿｽ�ｽ�ｽ�ｽﾎゑｿｽ�ｽ�ｽ
-		// �ｽm�ｽb�ｽN�ｽo�ｽb�ｽNenter�ｽ�ｽ�ｽﾉ敵�ｽ�ｽstartAttack()�ｽ�ｽ�ｽﾄべゑｿｽ,�ｽG�ｽﾆ敵�ｽﾌノ�ｽb�ｽN�ｽo�ｽb�ｽN�ｽ�ｽ�ｽ�ｽ�ｽ�ｽ�ｽﾂ能�ｽ�ｽ�ｽ�､
 	}
 }
 
 void AttackComponent::processAttackPlayer()
 {
-	// �ｽ�ｽ�ｽm�ｽ�ｽplayer�ｽﾌ取得
 	PlayerActor* player = static_cast<GamePlay*>(mOwner->getSequence())->getPlayer();
 
 	if (player->getHpComp()->isInvincible()) {
